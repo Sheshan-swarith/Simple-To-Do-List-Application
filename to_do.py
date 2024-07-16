26f36ade43291b978da06fa65275c0c497b9e6d5
Sheshan-swarith <sheshanswarith07@gmail.com>
2024-07-16 11:44:04 +0530

    Add files via upload

---
+++ to_do.py
@@ -0,0 +1,68 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python312/to_do.py ====
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 2
+Enter the task: market
+Task 'market' added to your to-do list.
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 2
+Enter the task: college
+Task 'college' added to your to-do list.
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 2
+Enter the task: home
+Task 'home' added to your to-do list.
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 3
+To-Do List:
+1. market (Not Done)
+2. college (Not Done)
+3. home (Not Done)
+Enter the task number to mark as completed: 1
+Task 1 marked as completed.
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 1
+To-Do List:
+1. market (Done)
+2. college (Not Done)
+3. home (Not Done)
+
+Options:
+1. Display to-do list
+2. Add a task
+3. Mark a task as completed
+4. Remove a task
+5. Quit
+Enter your choice: 5
